30a357b5f9318a2be8fbd2caf67ad06747ea5419
unknown <briandav22@gmail.com>
2020-12-22 09:25:29 -0500

    things

--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
 import psycopg2
 import ipaddress
 import csv
+import time
 class DB_handler:
     ## Handles the database connection
     def __init__(self,db_name,user,password,scrut_ip):
@@ -18,8 +19,6 @@ class DB_handler:
 
     def execute_query(self, query):
         self.cur.execute(query)
-        response = self.cur.fetchall()
-        print(response)
         try:
             record = [r[0] for r in self.cur.fetchall()]
             
@@ -27,6 +26,40 @@ class DB_handler:
             record = 'nothing_returned'
         self.conn.commit()
         return(record)
+
+    def execute_index_query(self,query):
+        time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))
+        
+        self.cur.execute(query)
+        response = self.cur.fetchall()
+
+        data_returned = []
+
+        try:
+            for result in response:
+                host_found = result[0]
+                exporter_seen_on = result[1]
+                connections = result[5]
+                first_seen = result[6]
+                last_seen = result[7]
+                first_seen = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(result[6]))
+                last_seen = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(result[7]))
+                returned_object = {
+                    'host_found':host_found,
+                    'exporter_seen_on':exporter_seen_on,
+                    'connections':connections,
+                    'first_seen':first_seen,
+                    'last_seen':last_seen
+                }
+                data_returned.append(returned_object)
+          
+        except:
+            record = 'nothing_returned'
+            pass
+        
+        self.conn.commit()
+
+        return(data_returned)      
 
     def close_connection(self):
         self.cur.close()
@@ -74,7 +107,7 @@ class Host_searcher():
         
 
 
-db_handler = DB_handler('plixer','root','admin','127.0.0.1')
+db_handler = DB_handler('plixer','scrutremote','admin','10.60.2.19')
 path_to_csv = '/home/plixer/scrutinizer/files/ipsearch/sunburst/allips.csv'
 
 
@@ -89,19 +122,19 @@ copy_csv = host_search.copy_csv('sunburst',path_to_csv)
 inner_join = host_search.inner_joins('sunburst')
 
 
-try:
-    db_handler.execute_query(create_table_query)
-    db_handler.execute_query(copy_csv)
+# try:
+#     db_handler.execute_query(create_table_query)
+#     db_handler.execute_query(copy_csv)
     
-except:
-    db_handler.close_connection()
+# except:
+#     db_handler.close_connection()
 
-    pass
+#     pass
 
 db_handler.open_connection()
 
 
 
-results = db_handler.execute_query(inner_join)
+results = db_handler.execute_index_query(inner_join)
 
 print(results)
